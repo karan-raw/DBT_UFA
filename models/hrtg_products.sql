19e9714d0bed79096c4d25b7d3f17e7dc36572be
Karan <karan.rawat@datachannel.co>
2025-04-29 11:21:20 +0530

    Products Changes

--- models/hrtg_products.sql
+++ models/hrtg_products.sql
@@ -1,5 +1,5 @@
-select t1.id, t1.title, t1.producttype, t1.updatedat,
-       t2.sku, t2.title, t2.price
+select t1.id, t1.title product_title, t1.producttype, t1.updatedat,
+       t2.sku, t2.title var_title, t2.price
 from dc_dc_frontendtest_003.shopgql_hrt_products t1
 join dc_dc_frontendtest_003.shopgql_hrt_products_variants t2
 on t1.id=t2.hrt_products_id
